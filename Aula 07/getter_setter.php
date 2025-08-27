a1a4305d405c46b30f56ff47f6f64e26f614530c
Gabriel <gabrielgislaine369@gmail.com>
2025-08-27 17:02:25 -0300

    exempols getters e setter 27/08 aula php

---
+++ Aula 07/getter_setter.php
@@ -0,0 +1,126 @@
+<?php
+
+class pessoa{
+    private $nome;
+    private $cpf;
+    private $telefone;
+    private $idade;
+    private $email;
+    private $senha;
+
+
+    // public function __construct($nome, $cpf, $telefone, $idade, $email, $senha) {
+    //     $this->nome = $nome;
+    //     $this->cpf = $cpf;
+    //     $this->telefone = $telefone;
+    //     $this->idade = $idade;
+    //     $this->email = $email;
+    //     $this->senha = $senha;
+    // }
+
+    public function __construct($nome, $cpf, $telefone, $idade, $email, $senha) {
+        $this->setNome($nome);
+        $this->setCpf($cpf);
+        $this->setTelefone($telefone);
+        $this->setIdade($idade);
+        $this->setEmail($email);
+        $this->setSenha($senha);
+    }
+
+
+    // getter e setter para $nome
+    public function getNome() {
+        return $this->nome;
+    }
+
+
+    public function setNome($novoNome) {
+        $this->nome = ucwords(strtolower($novoNome));
+    }
+    
+
+    // getter e setter para $cpf
+    public function getCpf() {
+        return $this->cpf;
+    }
+
+    public function setCpf($novoCpf) {
+        $this->cpf = preg_replace('/\D/', '', $novoCpf);
+    }
+
+
+    // getter e setter para $telefone
+    public function getTelefone() {
+        return $this->telefone;
+    }
+
+    public function setTelefone($novoTelefone) {
+        $this->telefone = preg_replace('/\D/', '', $novoTelefone);
+    }
+
+
+    // getter e setter para $idade
+    public function getIdade() {
+        return $this->idade;
+    }
+
+    public function setIdade($novaIdade) {
+        $this->idade = abs((int)$novaIdade,);   // abs: garante numero positivo
+    }
+
+
+    // getter e setter para $email
+    public function getEmail() {
+        return $this->email;
+    }
+
+    public function setEmail($novoEmail) {
+        $this->email = (string)$novoEmail;
+    }
+
+
+
+    // getter e setter para $senha
+    public function getSenha() {
+        return $this->senha;
+    }
+
+    public function setSenha($novaSenha) {
+        $this->senha = $novaSenha;
+    }
+
+
+
+    public static function criarViaTerminal() {
+        echo "Digite o nome: ";
+        $nome = readline();
+        echo "Digite o CPF: ";
+        $cpf = readline();
+        echo "Digite o telefone: ";
+        $telefone = readline();
+        echo "Digite a idade: ";
+        $idade = readline();
+        echo "Digite o email: ";
+        $email = readline();
+        echo "Digite a senha: ";
+        $senha = readline();
+
+        return new self($nome, $cpf, $telefone, $idade, $email, $senha);
+    }
+
+
+    public function exibirInfo() {
+        return "infromacões do usúario:\n Nome: $this->nome\n CPF: $this->cpf\n Telefone: $this->telefone\n Idade: $this->idade\n Email: $this->email\n Senha: $this->senha";  
+    }
+}
+
+
+$pessoa2 = new pessoa("gaBriEL dE OLIveirA gOMeS", "123.123.123-12", "(19) 982443230", -17, "gabriel.o.gomes1@aluno.senai.sp", 1234);
+
+$pessoa2 -> setNome("bRUNO alVEs");
+
+echo $pessoa2 -> getNome();
+
+
+$pessoa = pessoa::criarViaTerminal();
+echo $pessoa2 -> exibirInfo();
