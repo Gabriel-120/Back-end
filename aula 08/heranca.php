524e2441a9203c6a821270a5abbe36f5daf7bc2f
Gabriel <gabrielgislaine369@gmail.com>
2025-08-29 16:50:23 -0300

    exemplos de herança em php 29/08

--- aula 08/heranca.php
+++ aula 08/heranca.php
@@ -1,25 +1,46 @@
 <?php
 
-class veiculos{
-    private $marca;
-    private $modelo;
-    private $ano;
-    private $n_dono;
+class imovel{
+    private $categoria;
+    private $N_comodos;
+    private $valor;
+    private $estado_conservacao;
 
 
-    public function __construct($marca, $modelo, $ano, $n_dono){
-        $this->marca = $marca;
-        $this->modelo = $modelo;
-        $this->ano = $ano;
-        $this->n_dono = $n_dono;
+    public function __construct($categoria, $N_comodos, $valor, $estado_conservacao){
+        $this->categoria = $categoria;
+        $this->N_comodos = $N_comodos;
+        $this->valor = $valor;
+        $this->estado_conservacao = $estado_conservacao;
     }
 }
 
-class carro1 extends veiculos{
-    
+class casa extends imovel{
+    private $tem_quintal;
+
+    public function __construct($categoria, $N_comodos, $valor, $estado_conservacao, $tem_quintal){
+        parent::__construct($categoria, $N_comodos, $valor, $estado_conservacao);
+        $this->tem_quintal = $tem_quintal;
+    }
 }
 
 
-class moto1 extends veiculos {
+class apartamento extends imovel{
+    private $andar;
 
+    public function __construct($categoria, $N_comodos, $valor, $estado_conservacao, $andar){
+        parent::__construct($categoria, $N_comodos, $valor, $estado_conservacao);
+        $this->andar = $andar;
+    }
+}
+
+
+class escola extends imovel{
+    private $seguimento;
+
+    public function __construct($categoria, $N_comodos, $valor, $estado_conservacao, $seguimento){
+        parent::__construct($categoria, $N_comodos, $valor, $estado_conservacao);
+
+        $this->seguimento = $seguimento;
+    }
 }
