e951001aaac884ebbdff4072bc26865087577dba
Gabriel <gabrielgislaine369@gmail.com>
2025-10-24 15:51:31 -0300

    atividade json para armazenar dados 24/10 php

--- aula_12/alunoDAO.php
+++ aula_12/alunoDAO.php
@@ -1,27 +1,70 @@
 <?php
 
 // crud crete, read, update e delete
-class alunoDao{ //adao data access object
+class alunoDao
+{ //adao data access object
     private $alunos = []; // Array para armazenar temporáriamente os objetos e seus  atributos antes de mandar para o banco de dados. Foi criado inicialmente vazio[] ;
 
-    public function CriarAluno(Aluno $aluno){ // metodo create para criar um novo objeto
-        $this->alunos[$aluno->getId()] = $aluno;
+    private $arquivo = "alunos.json";
+
+    public function __construct()
+    {
+        if (file_exists($this->arquivo)) {
+            $conteudo = file_get_contents($this->arquivo);
+
+            $dados = json_decode($conteudo, true);
+
+            if ($dados) {
+                foreach ($dados as $id => $info) {
+                    $this->alunos[$id] = new Aluno(
+                        $info['id'],
+                        $info['nome'],
+                        $info['curso']
+                    );
+                }
+            }
+        }
     }
 
-    public function lerAluno(){  // método read  para ler informações de um objeto já existente
+    private function salvarEmArquivo()
+    {
+        $dados = [];
+
+        foreach ($this->alunos as $id => $aluno) {
+            $dados[$id] = [
+                'id' => $aluno->getId(),
+                'nome' => $aluno->getNome(),
+                'curso' => $aluno->getCurso()
+            ];
+        }
+
+        file_put_contents($this->arquivo, json_encode($dados, JSON_PRETTY_PRINT));
+    }
+
+
+    public function CriarAluno(Aluno $aluno)
+    { // metodo create para criar um novo objeto
+        $this->alunos[$aluno->getId()] = $aluno;
+        $this->salvarEmArquivo();
+    }
+
+    public function lerAluno()
+    {  // método read  para ler informações de um objeto já existente
         return $this->alunos;
     }
 
-    public function atualizarAluno($id, $novoNome, $novoCurso){ //método update para atualizar as informações de um obejeto já existente
+    public function atualizarAluno($id, $novoNome, $novoCurso)
+    { //método update para atualizar as informações de um obejeto já existente
         if (isset($this->alunos[$id])) {
             $this->alunos[$id]->setNome($novoNome);
             $this->alunos[$id]->setCurso($novoCurso);
-
         }
-        
+        $this->salvarEmArquivo();
     }
 
-    public function excluirAluno($id){ //método delete para excluir um objeto
+    public function excluirAluno($id){ 
+        //método delete para excluir um objeto
         unset($this->alunos[$id]);
+        $this->salvarEmArquivo();
     }
 }
